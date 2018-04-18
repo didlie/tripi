258c7d1ac5bea0b51113f62eaf4326e82a4a9280
Natalie Pearson <pearsonfam1@MacBook-Pro-51.local>
2018-04-18 16:16:08 -0700

    serversocket add file

---
+++ src/serverSocket.java
@@ -0,0 +1,36 @@
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Vector;
+
+@ServerEndpoint(value = "/ws")
+public class serverSocket {
+	private static Vector<Session> sessionVector = new Vector<Session>(); 
+	
+	@OnOpen 
+	public void open(Session session) {
+		System.out.println("Client connected");
+		sessionVector.add(session);
+	}
+	
+	@OnMessage 
+	public void onMessage(String message, Session session) {
+		System.out.println(message);
+		try {
+			for (Session s : sessionVector) {
+				s.getBasicRemote().sendText(message);
+			}
+		} catch (IOException ioe) {
+			System.out.println("ioe: " + ioe.getMessage());
+		}
+	}
+	
+	@OnClose 
+	public void close(Session session) {
+		System.out.println("Client disconnected");
+		sessionVector.remove(session);
+	}
+}
