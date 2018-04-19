39410110f6ed0e48b9a36abce1c25ab37ef92e59
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-04-18 23:26:07 -0700

    Added sample data to setup SQL file

--- tripi_setup.sql
+++ tripi_setup.sql
@@ -38,3 +38,11 @@ create table Item(
     FOREIGN KEY (trip_id) REFERENCES Trip(trip_id)
 );
 
+INSERT INTO `User` (`user_id`,`password`,`username`,`displayname`,`email`,`profile_pic`) VALUES (1,'password','WWillie','Willie','wwillie@usc.edu','https://pbs.twimg.com/profile_images/985671011965153280/CBqYLkWC_400x400.jpg');
+INSERT INTO `User` (`user_id`,`password`,`username`,`displayname`,`email`,`profile_pic`) VALUES (2,'fighton','JMiller','Jeffrey Miller PhD','jeffrey.miller@usc.edu','https://viterbi.usc.edu/directory/images/2dafc9bc59c736884ae57b7235d5bc67.jpg');
+
+INSERT INTO `Trip` (`trip_id`,`user_id`,`cover_photo_link`,`description`,`title`,`main_place`,`share`) VALUES (1,1,'http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1444253482/DG2015-new-york.jpg?itok=neFmsUY1','Leisurely tour of New York City','Big Apple Tour','New York',1);
+INSERT INTO `Trip` (`trip_id`,`user_id`,`cover_photo_link`,`description`,`title`,`main_place`,`share`) VALUES (2,1,'https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/lasvegas/strip_b86ddbea-3add-4995-b449-ac85d700b027.jpg','What happens in Vegas stays in Vegas','Sin City','Las Vegas',1);
+INSERT INTO `Trip` (`trip_id`,`user_id`,`cover_photo_link`,`description`,`title`,`main_place`,`share`) VALUES (3,2,'https://images.businessoffashion.com/site/uploads/2018/02/shutterstock_111362132.jpg?auto=format%2Ccompress&crop=top&fit=crop&h=573&w=1024','A tour of Paris, France','Tour de France','Paris',1);
+INSERT INTO `Trip` (`trip_id`,`user_id`,`cover_photo_link`,`description`,`title`,`main_place`,`share`) VALUES (4,2,'https://www.roadaffair.com/wp-content/uploads/2018/01/aerial-view-berlin-germany-shutterstock_337203833-780x520.jpg','Nice tour of Berlin, Germany','Field Trip to Berlin','Berlin',1);
+
